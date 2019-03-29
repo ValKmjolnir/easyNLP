0419ea34c390177b36a26e0d53f14db7ba8b7a58
Valk Richard Li <48872266+ValKmjolnir@users.noreply.github.com>
2019-03-29 12:47:25 +0800

    Create NLPann.h

---
+++ NLPann.h
@@ -0,0 +1,12 @@
+/*NLPann.h header file by ValK*/
+/*2019/3/29         version0.1*/
+#ifndef __NLPANN_H__
+#define __NLPANN_H__
+
+#include "bp.h"
+#include "rnn.h"
+#include "lstm.h"
+#include "gru.h"
+#include "activatefunction.h"
+
+#endif
